67a4c065f1ce209f8b5066ac572e831c19451220
bZverok <137277669+bZverok@users.noreply.github.com>
2024-08-26 16:13:25 +0300

    added app constants

---
+++ Apps.Akeneo/ApplicationConstants.cs
@@ -0,0 +1,7 @@
+namespace Apps.Akeneo;
+
+public static class ApplicationConstants
+{
+    public const string ClientId = "#{APP_AKENEO_CLIENT_ID}#";
+    public const string ClientSecret = "#{APP_AKENEO_SECRET}#";
+}
