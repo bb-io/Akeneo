c2f628bed1319d6f8a5e0d4ff75b3157ff2e6a6a
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-12-31 15:53:13 +0200

    Added debug action

---
+++ Apps.Akeneo/Actions/DebugActions.cs
@@ -0,0 +1,16 @@
+﻿using Apps.Akeneo.Invocables;
+using Blackbird.Applications.Sdk.Common.Actions;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Invocation;
+
+namespace Apps.Akeneo.Actions;
+
+[ActionList]
+public class DebugActions(InvocationContext invocationContext) : AkeneoInvocable(invocationContext)
+{
+    [Action("[DEBUG] Action", Description = "Debug action")]
+    public List<AuthenticationCredentialsProvider> DebugAction()
+    {
+        return Creds.ToList();
+    }
+}
