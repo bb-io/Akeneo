0082a044f4115f3586d7e11c76e8d179ff46c0d9
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-03-10 13:36:17 +0200

    add missing OAuth scopes

--- Apps.Akeneo/ApplicationConstants.cs
+++ Apps.Akeneo/ApplicationConstants.cs
@@ -4,5 +4,9 @@ public static class ApplicationConstants
 {
     //public const string ClientId = "#{AKENEO_CLIENT_ID}#";
     //public const string ClientSecret = "#{AKENEO_SECRET}#";
-    public const string Scopes = "write_products delete_products read_locales read_channel_localization read_catalog_structure read_attribute_options";
+    public const string Scopes =
+        "read_products write_products delete_products " +
+        "read_locales read_channel_localization read_catalog_structure write_catalog_structure " +
+        "read_attribute_options write_attribute_options " +
+        "read_categories write_categories ";
 }
